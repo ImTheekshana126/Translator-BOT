e54a104f0e77c49fc1e6b77fa52240b217df664d
Pawan Theekshana <87897206+ImTheekshana126@users.noreply.github.com>
2021-08-27 08:32:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,150 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Translator-Bot/blob/main/LICENSE
+
+import os
+from io import BytesIO
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from googletrans import Translator
+
+FayasNoushad = Client(
+    "Translator Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+START_TEXT = """
+Hello {}, I am a google translator telegram bot.
+Made by @FayasNoushad
+"""
+HELP_TEXT = """
+- Just send a text with language code
+example :- `This is a sample text | ml`
+Made by @FayasNoushad
+"""
+ABOUT_TEXT = """
+- **Bot :** `Translator Bot`
+- **Creator :** [Fayas](https://telegram.me/TheFayas)
+- **Channel :** [Fayas Noushad](https://telegram.me/FayasNoushad)
+- **Source :** [Click here](https://github.com/FayasNoushad/Translator-Bot)
+- **Language :** [Python3](https://python.org)
+- **Library :** [Pyrogram](https://pyrogram.org)
+- **Server :** [Heroku](https://heroku.com)
+"""
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Channel', url='https://telegram.me/FayasNoushad'),
+        InlineKeyboardButton('Feedback', url='https://telegram.me/TheFayas')
+        ],[
+        InlineKeyboardButton('Home', callback_data='home'),
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+CLOSE_BUTTON = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+TRANSLATE_BUTTON = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/FayasNoushad')
+        ]]
+    )
+DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")
+
+@FayasNoushad.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    else:
+        await update.message.delete()
+    
+
+@FayasNoushad.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TEXT.format(update.from_user.mention)
+    reply_markup = START_BUTTONS
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+@FayasNoushad.on_message((filters.private | filters.group | ~filters.channel) & filters.text)
+async def translate(bot, update):
+    if update.chat.type == "private":
+        if " | " in update.text:
+            text, language = update.text.split(" | ", 1)
+        else:
+            text = update.text
+            language = DEFAULT_LANGUAGE
+    else:
+        text = update.reply_to_message.text
+        if " " in update.text:
+            command, language = update.text.split(" | ", 1)
+        else:
+            language = DEFAULT_LANGUAGE
+    translator = Translator()
+    await update.reply_chat_action("typing")
+    message = await update.reply_text("`Translating...`")
+    try:
+        translate = translator.translate(text, dest=language)
+        translate_text = f"**Translated to {language}**"
+        translate_text += f"\n\n{translate.text}"
+        translate_text += "\n\nMade by @FayasNoushad"
+        if len(translate_text) < 4096:
+            await message.edit_text(
+                text=translate_text,
+                disable_web_page_preview=True,
+                reply_markup=TRANSLATE_BUTTON
+            )
+        else:
+            with BytesIO(str.encode(str(translate_text))) as translate_file:
+                translate_file.name = language + ".txt"
+                await update.reply_document(
+                    document=translate_file,
+                    caption="Made by @FayasNoushad",
+                    reply_markup=TRANSLATE_BUTTON
+                )
+                await message.delete()
+    except Exception as error:
+        print(error)
+        await message.edit_text("Something wrong. Contact @TheFayas.")
+        return
+
+FayasNoushad.run()
